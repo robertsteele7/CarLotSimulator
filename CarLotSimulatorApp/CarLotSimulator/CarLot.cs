4c9604962991a9edb967329261ba120a6bf0955c
robertsteele7 <atpcycle7@gmail.com>
2022-09-19 19:17:23 -0500

    Final Changes

---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+	class CarLot
+	{
+		public List<Car> CarList { get; set; } = new List<Car>();
+	}
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -8,7 +8,7 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
+            //Create a separate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
@@ -22,7 +22,44 @@ namespace CarLotSimulator
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
+            //Standard initialization
+           CarLot lot = new CarLot();
+
+            Car toyota = new Car();
+            lot.CarList.Add(toyota);
+            toyota.Year = 2010;
+            toyota.Make = "Prius";
+            toyota.Model = "Hybrid";
+            toyota.EngineNoise = "Barely";
+            toyota.HonkNoise = "weak beep beep";
+            toyota.IsDriveable = true;
+
+            toyota.MakeEngineNoise();
+            toyota.MakeHonkNoise();
+                
+
+            //Object initializer syntax
+            Car celica = new Car() { Year = 2010, Make = "Toyota", Model = "Celica", EngineNoise = "None", HonkNoise = "Sad" };
+            lot.CarList.Add(celica);
+
+            celica.MakeEngineNoise();
+            celica.MakeHonkNoise();
+
+            //Constructor initialization
+            Car someOtherCar = new Car(9999, "Venus Inc.", "Omega ++", "Mouth breathing", "What does a space alien eat?");
+            lot.CarList.Add(someOtherCar);
+
+            someOtherCar.MakeEngineNoise();
+            someOtherCar.MakeHonkNoise();
+
+           
             //*************BONUS X 2*************//
+
+            foreach(var car in lot.CarList)
+            { 
+                Console.WriteLine();
+                Console.WriteLine($"Year:{car.Year} Make:{car.Make} Model:{car.Model}");
+            }
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
